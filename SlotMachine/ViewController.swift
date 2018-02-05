8cbcb8bddf6dc2f524a4441b3772b4b4ad452be5
Sumit Ganju <sganju@Sumits-MacBook-Pro.local>
2018-02-05 14:05:53 -0500

    Added inline comments and additional functionality to the app.

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -1,13 +1,19 @@
-//
-//  ViewController.swift
-//  SlotMachine
-//
-//  Created by Sumit Ganju on 2018/1/29.
-//  Copyright © 2018 Centennial College. All rights reserved.
-//
+/*
+ * ViewController.swift
+ * Project: Assignment 1 - Swift Slot Machine
+ * Students:
+ *          Sumit Ganju                  300959971
+ *          Ling Bao                     300901785
+ *          Azeez adefarati Alaba        300971562
+ *
+ * Date: Feb 05, 2018
+ * Version: 0.1
+ */
 
 import UIKit
 extension String {
+    
+    // Convert the emojis we copy into a String to a UIImage
     func emojiToImage() -> UIImage? {
         let size = CGSize(width: 30, height: 35)
         UIGraphicsBeginImageContextWithOptions(size, false, 0)
@@ -21,7 +27,8 @@ extension String {
     }
 }
 class ViewController: UIViewController ,UIPickerViewDelegate, UIPickerViewDataSource{
-
+    
+    // OUTLETS
     @IBOutlet weak var winMoney: UILabel!
     @IBOutlet weak var ownMoney: UILabel!
     @IBOutlet weak var pickerView: UIPickerView!
@@ -30,9 +37,9 @@ class ViewController: UIViewController ,UIPickerViewDelegate, UIPickerViewDataSo
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       // Do any additional setup after loading the view.
         
-        
-
+        // REEL IMAGES
         imageArray = ["🍎".emojiToImage()!,
                       "😍".emojiToImage()!,
                       "🐮".emojiToImage()!,
@@ -47,12 +54,18 @@ class ViewController: UIViewController ,UIPickerViewDelegate, UIPickerViewDataSo
         resultLabel.text = ""
         winMoney.text = "0"
         ownMoney.text = "5000"
-      
+        
+        // Function to seed the random number generator
         arc4random_stir()
         pickerView.delegate = self
         pickerView.dataSource = self
+    
     }
-    @IBAction func buttonClicked(_ sender: UIButton) {
+    
+
+    // ACTIONS
+    // The Spin() method executes when the user taps the Spin button
+       @IBAction func buttonClicked(_ sender: UIButton) {
         var win = false
         var numInRow = -1
         var lastVal = -1
@@ -80,12 +93,15 @@ class ViewController: UIViewController ,UIPickerViewDelegate, UIPickerViewDataSo
                 win = true
             }
     }
+            
         resultLabel.text = win ? "WINNER!!!!" : ""
         if ownnedMoney == 0{
             resultLabel.text = "You Lose!"
         }
     }
-    }
+}
+    
+    // Picker Data Source Methods
     // function which returns the number of 'columns' to display.
     @available(iOS 2.0, *)
     public func numberOfComponents(in pickerView: UIPickerView) -> Int{
@@ -98,15 +114,17 @@ class ViewController: UIViewController ,UIPickerViewDelegate, UIPickerViewDataSo
     public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
         return imageArray.count
     }
-
+    // Setting the Width of your picker's components
     func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
         return 32
     }
     
+    // Picker Delegate Methods
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat{
         return 32
     }
     
+    // This Method returns one UIImageView object initialized with one of the images for the symbols
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
         let image = imageArray[row]
         let imageView = UIImageView(image: image)
